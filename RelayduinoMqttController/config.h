daefac20fd3a01d33c759c39f08280e7c99e9b41
Philip Cutler <greenthegarden@gmail.com>
2016-09-24 14:11:58 +0930

    updated README and support for PlatformIO

--- RelayduinoMqttController/config.h
+++ RelayduinoMqttController/config.h
@@ -1,15 +1,14 @@
 #ifndef RELAYDUINOMQTTCONTROLLER_CONFIG_H_
 #define RELAYDUINOMQTTCONTROLLER_CONFIG_H_
 
-
 #include "Debug.h"
 
-#define VERSION_MAJOR 3
-#define VERSION_MINOR 1
+#ifndef VERSION
+#define VERSION "1.0"
+#endif
 
 // external libraries
 #include <MemoryFree.h>
-//#include <SwitchedDevice.h>
 #include <Relayduino.h>
 
 // Macros
@@ -17,14 +16,14 @@
 
 // global variable definitions
 #if DEBUG_LEVEL > 0
-const int BAUD_RATE               = 9600;
+const int BAUD_RATE = 9600;
 #endif
 
-const byte BUFFER_SIZE            = 32;
+const byte BUFFER_SIZE = 32;
 char topicBuffer[BUFFER_SIZE];
 char payloadBuffer[BUFFER_SIZE];
 
-const unsigned long STATUS_UPDATE_INTERVAL = 5UL * 60UL *1000UL;  // 5 minutes
+const unsigned long STATUS_UPDATE_INTERVAL = 5UL * 60UL * 1000UL; // 5 minutes
 unsigned long statusPreviousMillis = 0UL;
 
 #include "ethernetConfig.h"
@@ -32,21 +31,22 @@ unsigned long statusPreviousMillis = 0UL;
 #include "alarmConfig.h"
 #include "relayConfig.h"
 
-void no_network_behaviour() {
-  relays_switch_off();
-}
+void no_network_behaviour() { relays_switch_off(); }
 
+#ifndef USE_INPUTS
 #define USE_INPUTS false
+#endif
 
 #if USE_INPUTS
-const byte OPTICAL_INPUTS[] = { OPTO_INPUT_1, OPTO_INPUT_2, OPTO_INPUT_3, OPTO_INPUT_4 };
-const byte ANALOG_INPUTS[]= { ANALOG_IN_1, ANALOG_IN_2, ANALOG_IN_3 };
+const byte OPTICAL_INPUTS[] = {OPTO_INPUT_1, OPTO_INPUT_2, OPTO_INPUT_3,
+                               OPTO_INPUT_4};
+const byte ANALOG_INPUTS[] = {ANALOG_IN_1, ANALOG_IN_2, ANALOG_IN_3};
 unsigned long inputsPreviousMillis = 0UL;
 const unsigned long INPUT_READ_INTERVAL = 5UL * 60UL * 1000UL;
 
 void publish_reading(byte inputType, byte idx, byte reading) {
   topicBuffer[0] = '\0';
-  strcpy_P(topicBuffer, (char*)pgm_read_word(&(INPUT_TOPICS[inputType])));
+  strcpy_P(topicBuffer, (char *)pgm_read_word(&(INPUT_TOPICS[inputType])));
   payloadBuffer[0] = '\0';
   sprintf(payloadBuffer, "%i%c%i", idx, ',', reading);
   mqttClient.publish(topicBuffer, payloadBuffer);
@@ -64,7 +64,6 @@ void read_inputs() {
     publish_reading(OPTO_INPUT_IDX, idx, reading);
   }
 }
-#endif  /* USE_INPUTS */
+#endif /* USE_INPUTS */
 
-
-#endif   /* RELAYDUINOMQTTCONTROLLER_CONFIG_H_ */
+#endif /* RELAYDUINOMQTTCONTROLLER_CONFIG_H_ */

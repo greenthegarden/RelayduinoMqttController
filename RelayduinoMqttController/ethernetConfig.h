a9fb844b40992b9f44df47fc629f917d5e1e4b90
Philip Cutler <cutlervufamily@gmail.com>
2023-09-23 11:16:27 +0930

    added mqtt address comment

--- RelayduinoMqttController/ethernetConfig.h
+++ RelayduinoMqttController/ethernetConfig.h
@@ -3,19 +3,17 @@
 
 // Ethernet libraries
 #include <SPI.h>
-#include <Ethernet3.h>
+#include <Ethernet.h>
 
 #ifndef ETHERNET_SHIELD_VERSION
-#define ETHERNET_SHIELD_VERSION 2
+#define ETHERNET_SHIELD_VERSION 1
 #endif
 
 #if ETHERNET_SHIELD_VERSION == 1
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE };
 #elif ETHERNET_SHIELD_VERSION == 2
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0xFC, 0xA9 };
 #endif
-
-IPAddress ip(192, 168, 1, 250);
 
 EthernetClient ethernetClient;
 
@@ -23,10 +21,12 @@ const unsigned long NETWORK_STARTUP_DELAY = 1500UL;
 
 void ethernet_init()
 {
+  DEBUG_LOG(1, "Configuring Ethernet")
   // Configure Ethernet
   if (Ethernet.begin(mac) == 0) {
     DEBUG_LOG(1, "Failed to configure Ethernet using DHCP");
     // try to configure using IP address instead of DHCP:
+    IPAddress ip(192, 168, 1, 250);
     Ethernet.begin(mac, ip);
   }
   delay(NETWORK_STARTUP_DELAY);
